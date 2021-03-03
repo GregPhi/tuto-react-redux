f4714c59d5913f70f9bfc103375c4baac5a1fa0b
GregPhi <gregoire.philippot@laposte.net>
2021-03-03 12:19:09 +0100

    fix: create first reducer
    init an object animals contains a list of animal
    init an id
    
    create a function use to select the good action (ADD, UPDATE, DELETE, ...) -> for now just ADD is implement
    
    create 2 const :
    - firstAnimal -> use to see animals without difference
    - newAnimal -> use to see animals with ADD action

--- redux-tuto/src/index.js
+++ redux-tuto/src/index.js
@@ -1,3 +1,41 @@
+console.log("Tutorial redux");
+
+let animalId = 2
+const animals = [
+  {
+    animalId: 1,
+    name: "Tiger",
+    favorite: false
+  },
+  {
+    animalId: 2,
+    name: "Panda",
+    favorite: false
+  }
+]
+
+const ADD_ANIMAL_ACTION = "ADD_ANIMAL_ACTION"
+const UPDATE_ANIMAL_ACTION = "UPDATE_ANIMAL_ACTION"
+const DELETE_ANIMAL_ACTION = "DELETE_ANIMAL_ACTION"
+
+function animalsRedecuer(state = animals, action) {
+  switch (action.type) {
+    case ADD_ANIMAL_ACTION:
+      return [...state, {animalId: ++animalId, ...action.payload , favorite:false}]
+    case UPDATE_ANIMAL_ACTION:
+      break;
+    case DELETE_ANIMAL_ACTION:
+      break;
+    default:
+      return state
+  }
+}
+
+const firstAnimal = animalsRedecuer(undefined, {})
+const newAnimal = animalsRedecuer(animals, {type:ADD_ANIMAL_ACTION, payload: {name:"Tiber"}})
+console.log(firstAnimal, newAnimal);
+
+/**
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
@@ -15,3 +53,4 @@ ReactDOM.render(
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
+**/
