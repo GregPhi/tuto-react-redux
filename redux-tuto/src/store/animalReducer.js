4a8bc57e9caadeb9dd77242ddc4403bd4ac56117
GregPhi <gregoire.philippot@laposte.net>
2021-03-03 16:51:24 +0100

    fix: fix import and implement delete action

--- redux-tuto/src/store/animalReducer.js
+++ redux-tuto/src/store/animalReducer.js
@@ -1,4 +1,4 @@
-import { ADD_ANIMAL_ACTION, UPDATE_ANIMAL_ACTION, DELETE_ANIMAL_ACTION } from './action.js'
+import * as ActionType from './actionType.js'
 
 let animalId = 3
 const animals = [
@@ -21,9 +21,9 @@ const animals = [
 
 export function animalReducer(state = animals, action) {
   switch (action.type) {
-    case ADD_ANIMAL_ACTION:
+    case ActionType.ADD_ANIMAL_ACTION:
       return [...state, {animalId: ++animalId, ...action.payload , favorite:false}]
-    case UPDATE_ANIMAL_ACTION:
+    case ActionType.UPDATE_ANIMAL_ACTION:
       return state.map(animal => {
         if(animal.animalId === action.payload.animalId){
           return {...animal, ...action.payload}
@@ -31,8 +31,8 @@ export function animalReducer(state = animals, action) {
           return animal
         }
       })
-    case DELETE_ANIMAL_ACTION:
-      break;
+    case ActionType.DELETE_ANIMAL_ACTION:
+      return state.filter(animal => animal.animalId !== action.payload)
     default:
       return state
   }
