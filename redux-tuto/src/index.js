b40aa84b762a9be27200f7b48ea24f1236d4bcb9
GregPhi <gregoire.philippot@laposte.net>
2021-03-03 12:26:38 +0100

    fix : create a store
    delete old implementation -> use a store
    
    create const store with createStore(reducer, ...) function
    it's a store redux (with a parameter to see Redux inspector on firefox)
    
    make a subscribe on store -> when modification on store make an action (console.log to see the state)
    make 2 dispatch to add animal in store

--- redux-tuto/src/index.js
+++ redux-tuto/src/index.js
@@ -1,3 +1,5 @@
+import { createStore } from 'redux'
+
 console.log("Tutorial redux");
 
 let animalId = 2
@@ -31,12 +33,21 @@ function animalsRedecuer(state = animals, action) {
   }
 }
 
-console.log("Animals : ");
-const firstAnimal = animalsRedecuer(undefined, {})
-console.log(firstAnimal);
-console.log("\n Animals with ADD");
-const newAnimal = animalsRedecuer(animals, {type:ADD_ANIMAL_ACTION, payload: {name:"Tiber"}})
-console.log(newAnimal);
+const store = createStore(
+  animalsRedecuer,
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+) // store redux
+/**
+onglet redux :
+gauche -> les actions effectuées (nom clair)
+droite ->
+Action :
+State : état du store
+Diff : ce qu'a fait l'action
+**/
+store.subscribe(() => console.log(store.getState())) // permettre écout lors changement sur le store -> fonction qui effectue une action lors d'un changement
+store.dispatch({type:ADD_ANIMAL_ACTION, payload: {name:"Tiber"}}) // envoi une action (ici ADD)
+store.dispatch({type:ADD_ANIMAL_ACTION, payload: {name:"Snake"}})
 
 /**
 import React from 'react';
