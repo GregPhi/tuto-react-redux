c81365288f02556ecfa9044724dcaa565d8fd4b0
GregPhi <gregoire.philippot@laposte.net>
2021-03-03 16:49:58 +0100

    fix: update action can be use
    create method add animal
    update method delete animal

--- redux-tuto/src/store/animalActions.js
+++ redux-tuto/src/store/animalActions.js
@@ -1,6 +1,16 @@
-import { UPDATE_ANIMAL_ACTION } from './action.js'
+import * as ActionType from './actionType.js'
+
+export const addAnimalAction = (name) => ({
+  type: ActionType.ADD_ANIMAL_ACTION,
+  payload: {name}
+})
 
 export const toggleAnimalAction = (animal) => ({
-  type: UPDATE_ANIMAL_ACTION,
+  type: ActionType.UPDATE_ANIMAL_ACTION,
   payload: {...animal, favorite:!animal.favorite}
+})
+
+export const deleteAnimalAction = (animal) => ({
+  type: ActionType.DELETE_ANIMAL_ACTION,
+  payload: animal.animalId
 })
