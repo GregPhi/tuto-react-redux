de0612897f38df6c4b47937da463e2dd32043253
GregPhi <gregoire.philippot@laposte.net>
2021-03-04 14:29:36 +0100

    fix: add comment to delete warning when server is start

--- redux-tuto/src/components/AddAnimal.jsx
+++ redux-tuto/src/components/AddAnimal.jsx
@@ -5,7 +5,7 @@ import NewAnimalForm from '../generics/NewAnimalForm'
 
 export function AddAnimalForm (){
   const dispatch = useDispatch()
-
+  // eslint-disable-next-line
   const handleSubmit = useCallback( animal => {dispatch(addAnimalAction(animal.name))}, [])
   return <NewAnimalForm onAdd={handleSubmit} />
 }
